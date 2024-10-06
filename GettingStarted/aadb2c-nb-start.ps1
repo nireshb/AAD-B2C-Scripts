8649040e2e704ed967d6c3466fecafb8ecae20c1
Nireshen Beerbul <nireshen@gmail.com>
2024-10-06 14:52:23 +0200

    Add script order

--- GettingStarted/aadb2c-nb-start.ps1
+++ GettingStarted/aadb2c-nb-start.ps1
@@ -3,5 +3,5 @@ Import-Module ./Az.ADB2C.psm1
 Connect-AzADB2C -ConfigPath ./b2cAppSettings_a73csygrwfojk.json 
 Enable-AzADB2CIdentityExperienceFramework
 New-AzADB2CLocalAdmin -u "graphexplorer" -RoleNames @("Global Administrator")
-rem New-AzADB2CLocalAdmin -u "alice@contoso.com" -DisplayName "Alice Contoso" -RoleNames @()
+# New-AzADB2CLocalAdmin -u "alice@contoso.com" -DisplayName "Alice Contoso" -RoleNames @()
 New-AzADB2CTestApp -DisplayName a73csygrwfojkTestApp
